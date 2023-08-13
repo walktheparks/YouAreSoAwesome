5d47e53ee7e22d95cec8fec1d70c3f9b0490423b
Blake Robinson <robinson.blake@gmail.com>
2023-08-13 11:30:52 -0700

    Initial Commit

---
+++ YouAreSoAwesome/YouAreSoAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreSoAwesomeApp.swift
+//  YouAreSoAwesome
+//
+//  Created by Blake Robinson on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreSoAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
